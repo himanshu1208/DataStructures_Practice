d092d705bb6fe76b5e4f78db7b87ac2b705f4feb
Himanshu <himanshumathur1208@gmail.com>
2022-05-07 21:33:10 +0530

    Custom Hashtable complete

--- code/hashMap.js
+++ code/hashMap.js
@@ -3,7 +3,7 @@ class HashTable {
       this.data = new Array(size);
     }
   
-    _hash(key) {
+     _hash(key) {
       let hash = 0;
       for (let i =0; i < key.length; i++){
           hash = (hash + key.charCodeAt(i) * i) % this.data.length
@@ -12,16 +12,30 @@ class HashTable {
     }
 
     set(key,value) {
-      this.data[this._hash(key)] = value;
+      // let address = this_hash(key);
+      if(!this.data[this._hash(key)]) {
+        this.data[this._hash(key)] = [];
+      }
+      this.data[this._hash(key)].push([key,value])
     }
 
     get(key) {
-      return this.data[this._hash(key)]
+      let address = this._hash(key);
+      const databucket = this.data[address];
+      if(databucket.length) {
+        for (var i=0; i<databucket.length; i++) {
+          if(databucket[i][0] === key) {
+            return databucket[i][1];
+          }
+        }
+      }   
+      return "Not Available in the map";
     }
   }
   
-  const myHashTable = new HashTable(50);
+  const myHashTable = new HashTable(2);
   (myHashTable.set('grapes', 10000));
   console.log(myHashTable.get('grapes'));
   (myHashTable.set('apples', 9));
   console.log(myHashTable.get('apples'));
+  console.log(myHashTable.data);
